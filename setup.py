32ca08f103b85219a8f78e16fd06000d82aea436
Samantha Pendleton <samanfapendle@outlook.com>
2020-02-20 13:13:49 +0000

    included additional stop word

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
       name='jabberwocky',
-      version='0.4.0.2',
+      version='0.4.0.3',
       description='tool for ontologies',
       url='https://github.com/sap218/jabberwocky',
       author='Samantha C Pendleton',
