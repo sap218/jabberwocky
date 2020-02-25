9b152025f7faf303c9541edaeccec20e5360cc2e
Samantha Pendleton <samanfapendle@outlook.com>
2020-02-25 10:51:45 +0000

    PRESENTING ARISE

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
       name='jabberwocky',
-      version='0.4.0.3',
+      version='0.5.0.0',
       description='tool for ontologies',
       url='https://github.com/sap218/jabberwocky',
       author='Samantha C Pendleton',
