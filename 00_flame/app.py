a1baa6d3c0f7afd7ae789c6e841f79c9ebaeacac
Samantha Pendleton <samanfapc@gmail.com>
2026-09-13 22:20:12 +0100

    inital files

---
+++ 00_flame/app.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  7 21:42:15 2026
+
+@author: Sam
+"""
+
+import streamlit as st
+
+# !python -m streamlit run app.py
+
+st.title("Jabberwocky")
+
+st.write("@ sap218")
+
+
+
---
+++ 00_flame/run.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  7 21:44:52 2026
+
+@author: Sam
+"""
+
+import os
+
+os.system("python -m streamlit run app.py")
+
+
+# Anaconda Prompt
+# 
